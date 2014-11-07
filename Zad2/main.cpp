5598c2463e8279ada1f8b6ef76a2915855584d3e
andonnnn1 <andonnnn1@abv.bg>
2014-11-07 09:09:47 +0200

    second week

---
+++ Zad2/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    cin>>c;
+    if (c>='0' && c<='9') {
+        cout<<"number";
+        }
+        else
+        { if (c>='a' && c<='z'
+              {
+                  cout <<"malka bykva";
+              }
+              else
+              {if (c>='A' && <='z')
+              {
+                  cout <<"golqma bykva;
+              }
+              else
+              {
+return 0;
+}
+
+
