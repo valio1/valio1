e7011bf76a7167f248870fc61da43deb846ce9e7
mladengsi <mladen_gsi@abv.bg>
2014-11-14 09:14:39 +0200

    week 3

---
+++ 123456.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    string Alexander,Georgi,Petar,Ivan,Atanas,Valentin,Stanimir,Stanislav,Aben,Albert,Alben;
+
+    cout<<"Name 1"<<endl;
+    getline (cin,Alexander);
+    cout<<Alexander<<endl;
+
+    cout<<"Name 2"<<endl;
+    getline (cin,Georgi);
+    cout<<Georgi<<endl;
+
+    cout<<"Name 3"<<endl;
+    getline (cin,Petar);
+    cout<<Petar<<endl;
+
+    cout<<"Name 4"<<endl;
+    getline (cin,Ivan);
+    cout<<Ivan<<endl;
+
+    cout<<"Name 5"<<endl;
+    getline (cin,Atanas);
+    cout<<Atanas<<endl;
+
+    cout<<"Name 6"<<endl;
+    getline (cin,Valentin);
+    cout<<Valentin<<endl;
+
+    cout<<"Name 7"<<endl;
+    getline (cin,Stanimir);
+    cout<<Stanimir<<endl;
+
+    cout<<"Name 8"<<endl;
+    getline (cin,Stanislav);
+    cout<<Stanislav<<endl;
+
+    cout<<"Name 9"<<endl;
+    getline (cin,Aben);
+    cout<<Aben<<endl;
+
+    cout<<"Name 10"<<endl;
+    getline (cin,Albert);
+    cout<<Albert<<endl;
+
+    cout<<"Name 11"<<endl;
+    getline (cin,Alben);
+    cout<<Alben<<endl;
+
+    return 0;
+}
