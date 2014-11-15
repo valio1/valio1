487737e66be9841926497abb251a7939f54e562c
mladengsi <mladen_gsi@abv.bg>
2014-11-15 16:41:42 +0200

    zadacha4

---
+++ main (2).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //uhuh
+    cout<<"Valentin\nPetar\nIvan\nGeorgi\nViktor"<<endl; //huhii
+    //fdfhfkk
+    /*gh;h
+    jhpjj
+    j;oh;h
+    hhpup
+    */
+    return 0;
+}
