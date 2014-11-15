291b1ccd3651351102e533d13d950d8a6022d0c0
mladengsi <mladen_gsi@abv.bg>
2014-11-15 17:26:08 +0200

    zadacha5

---
+++ BeABootlicker/main.cpp
@@ -0,0 +1,22 @@
+//tove e bibliotekata
+#include <iostream>
+#include <string>
+
+using namespace std;
+//izvejda dymi
+int main()
+{   //deklarirame nqkakva promenliva
+    string myFavoriteFilm;
+    //izvejdame nqkak1v tekst
+    cout<<"Vavedi svoq lubim film"<<endl;
+    //v1vejda nqkak1v tekst
+    cin>>myFavoriteFilm;
+    //izvejda twkst
+    cout<<"Tvoqt lubim film e "<<myFavoriteFilm;
+
+    return 0;
+}
+
+
+
+
