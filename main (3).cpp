1e267e042be47246fca44ae3cdbb8a4f60e4ef8b
mladengsi <mladen_gsi@abv.bg>
2014-11-21 09:05:52 +0200

    zad 4

---
+++ main (3).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a[3][4];
+   for (int i = 0; i < 3; i++);
+{
+for (int j = 0; j < 4; j++);
+{
+cout << "a [ " << i << " ] [ " << j << " ] = ";
+cin >> a[ i ][ j ];
+}
+}
+return o;
+}
+
